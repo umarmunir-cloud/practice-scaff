b14aac634aa4c33605b14c5de89e059034ab283b
umarmunir-cloud <umar.munir@tuf.edu.pk>
2026-02-27 11:57:02 +0500

    Database Updated

--- database/migrations/2026_02_27_113550_create_manager_posts_table.php
+++ database/migrations/2026_02_27_113550_create_manager_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
 
             $table->string('name')->unique();
             $table->string('slug')->unique();
-            $table->text('description')->nullable();
+            $table->string('image')->nullable();
 
             $table->timestamps();
 
